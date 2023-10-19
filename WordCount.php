74c9185d034c77dfb3fe670f9811c284fb899dfb
Hafidzul Ahkam Dwika Maulana <ghafid999@gmail.com>
2023-10-19 14:25:39 +0700

    Tugas Unit Testing WKPL 19/10/2023

--- WordCount.php
+++ WordCount.php
@@ -3,6 +3,8 @@ class WordCount
 {
     public function countWords($sentence)
     {
+        // explode berfungsi untuk memecah kalimat berdasarkan spasi
+        // count berfungsi untuk menghitung kata yang telah dipecah oleh explode
         return count(explode(" ", $sentence));
     }
 }
